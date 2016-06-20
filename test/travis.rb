c8ac073d0ac9fd436005d38794b021da4d686186
Joseph Fusco <hello@josephfus.co>
2016-06-20 00:58:30 -0400

    More tests

--- test/travis.rb
+++ test/travis.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
-result = `sass test/test.scss built.css`
+result = `sass test/test.scss test.css`
 raise result unless $?.to_i == 0
-raise "When compiled the module should output some CSS" unless File.exists?('built.css')
-puts "Regular compile worked successfully"
+raise "When compiled the module should output some CSS" unless File.exists?('test.css')
+puts "SASS compile was successful."
